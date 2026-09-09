7fc94612dfeb6b587c14ab46bbc04277716d56a3
rshd123 <arshadm1512@gmail.com>
2026-09-09 22:04:50 +0530

    backend: lang update

--- backend/src/config/pythonService.js
+++
@@ -1,9 +0,0 @@
-const axios = require('axios');
-
-const pythonService = axios.create({
-  baseURL: process.env.PYTHON_SERVICE_URL || 'http://localhost:8000',
-  timeout: 30000,
-  headers: { 'Content-Type': 'application/json' }
-});
-
-module.exports = pythonService;
--- backend/src/routes/api.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const queryController = require('../controllers/query.controller');
-const graphController = require('../controllers/graph.controller');
-
-router.post('/query', queryController.handleQuery);
-router.get('/graph', graphController.getGraph);
-
-module.exports = router;
